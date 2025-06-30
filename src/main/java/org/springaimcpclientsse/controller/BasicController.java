47b3b938b1b7590026685fc6e90a9a9ff2f564b5
Ajeet Singh <ajeet.cloud@gmail.com>
2025-07-01 00:15:26 +0530

    Updated endpoint name

--- src/main/java/org/springaimcpclientsse/controller/BasicController.java
+++ src/main/java/org/springaimcpclientsse/controller/BasicController.java
@@ -6,6 +6,7 @@ import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,13 +23,13 @@ public class BasicController {
                 .build();
     }
 
-    @GetMapping("/basicChat")
-    public List<Generation> basicChat() {
-        PromptTemplate promptTemplate = new PromptTemplate("My current location is latitude 20 and longitude 20. Can you let me know if this weather is fine for playing tennis");
+    @GetMapping("/chat")
+    public String chat(@RequestParam String query) {
+        PromptTemplate promptTemplate = new PromptTemplate(query);
         Prompt prompt = promptTemplate.create();
         ChatClient.CallResponseSpec res = chatClient.prompt(prompt).call();
 
-        return res.chatResponse().getResults();
+        return res.content();
     }
 
 }
